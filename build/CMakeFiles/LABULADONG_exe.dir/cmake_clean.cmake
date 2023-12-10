58ca5d449a19dccb6b617a0a90df9e151b2a3f45
Squirrelslee <1525076988@qq.com>
2023-12-10 16:10:03 +0800

    1

---
+++ build/CMakeFiles/LABULADONG_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LABULADONG_exe.dir/labuladong.cc.o"
+  "LABULADONG_exe"
+  "LABULADONG_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LABULADONG_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/LABULADONG_exe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LABULADONG_exe.
+# This may be replaced when dependencies are built.
