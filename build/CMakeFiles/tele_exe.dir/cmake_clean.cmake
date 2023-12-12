7db8686891dd65df89d70a5d25a46fb2993639ee
Squirrelslee <1525076988@qq.com>
2023-12-12 22:27:09 +0800

    1

---
+++ build/CMakeFiles/tele_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tele_exe.dir/teleoperation.cc.o"
+  "tele_exe"
+  "tele_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tele_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
