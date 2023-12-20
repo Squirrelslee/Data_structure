509ec518c86bd6fdd2738a7abdd9c2082c774a5b
Squirrelslee <1525076988@qq.com>
2023-12-20 15:17:01 +0800

    1

---
+++ build/CMakeFiles/leet_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leet_exe.dir/leetcode.cc.o"
+  "leet_exe"
+  "leet_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leet_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
